cb679c9077b1b623633a93d42dc91d4d5b6a2b47
Sebastian Pacheco <sebastianpachecogomez@gmail.com>
2017-07-28 11:15:24 -0500

    Fixing the wrong import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { HttpModule } from '@angular/http';
 
 // Imports for loading & configuring the in-memory web api
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppRoutingModule }     from './app-routing.module';
 
